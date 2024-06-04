8a8eb8be587f3ebc790627da92ce8406b6e68f9e
LLauriB <166570088+LLauriB@users.noreply.github.com>
2024-06-04 13:24:52 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function cambiar(element){
+    element.innerText="Logout";
+    
+}
+
+function messageAlert(){
+    window.alert("Ninja was liked")
+}
+
+function removerButton(element){
+    element.remove()
+}
